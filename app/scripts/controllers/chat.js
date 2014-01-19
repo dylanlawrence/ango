40f7801dc8c646a1b4a46ca8966ad4251cfa2442
Dylan Lawrence <dylanslawrence@gmail.com>
2014-01-18 19:29:37 -0800

    chat

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -1,8 +1,43 @@
 'use strict';
 
-angular.module('angoApp')
-  .controller('ChatCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  }]);
+angular.module('angoApp').controller('ChatCtrl', ['$scope', '$http', 'Global', 'Socket', 'Users', function($scope, $http, Global, Socket, Users) {
+
+	$scope.global = Global;
+
+	$scope.messages = [];
+
+	socket.on('user:connected', function() {
+		//alert('user:connected');
+		$scope.getUsers();
+	})
+
+	socket.on('user:disconnected', function() {
+		//alert('user:disconnected');;
+		$scope.getUsers();
+	});
+
+	$scope.send = function() {
+		socket.emit('send:message', {
+			user: $scope.global.user._id,
+			text: $scope.message
+		});
+		// add the message to our model locally
+		$scope.messages.push({
+			user: $scope.global.user._id,
+			text: $scope.message
+		});
+		// clear 
+		$scope.message = '';
+	};
+
+
+	$scope.getUsers = function() {
+		Users.query({
+			userId: 'all'
+		}, function(users) {
+			$scope.users = users;
+		});
+	};
+
+
+}]);
--- app/scripts/services/global.js
+++ app/scripts/services/global.js
@@ -13,4 +13,5 @@ angular.module('angoApp')
         return meaningOfLife;
       }
     };
+    
   }]);
